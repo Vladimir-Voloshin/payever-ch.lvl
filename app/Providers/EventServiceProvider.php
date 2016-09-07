c6f2318bb04cd60ffa0c65aad14793be11eea4d5
Boris Doomer <doomer@localhost.localdomain>
2016-09-07 20:19:28 +0300

    migrate to 5.3

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -23,9 +23,9 @@ class EventServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(DispatcherContract $events)
+    public function boot()
     {
-        parent::boot($events);
+        parent::boot();
 
         //
     }
